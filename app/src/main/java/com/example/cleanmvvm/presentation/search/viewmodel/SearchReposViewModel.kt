3e8c5e6c964b7eb1c6129268de9710a053961a49
mobilestudio00 <jogarcia2411@gmal.com>
2020-02-01 13:53:10 -0600

    se agrego la base de datos

--- app/src/main/java/com/example/cleanmvvm/presentation/search/viewmodel/SearchReposViewModel.kt
+++ app/src/main/java/com/example/cleanmvvm/presentation/search/viewmodel/SearchReposViewModel.kt
@@ -2,31 +2,44 @@ package com.example.cleanmvvm.presentation.search.viewmodel
 
 
 import androidx.databinding.ObservableField
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cleanmvvm.data.local.RepoRoomDatabase
+import com.example.cleanmvvm.data.local.entity.UserRepo
+import com.example.cleanmvvm.data.local.repository.UserRepoRepository
+import com.example.cleanmvvm.data.network.models.response.GithubReponse
 import com.example.cleanmvvm.domain.model.Repo
 import com.example.cleanmvvm.domain.repositories.UserRepository
 import com.example.cleanmvvm.domain.usecases.GitHubUserUseCase
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SearchReposViewModel(
-    val userRepository: GitHubUserUseCase
+    val userRepository: GitHubUserUseCase,
+    private val repoRoomDatabase: RepoRoomDatabase
 ):ViewModel(){
 
 
     private val compositeDisposable:CompositeDisposable = CompositeDisposable()
     val repoMutableLiveData = MutableLiveData<List<Repo>>()
 
-     val userInput =ObservableField<String>("")
+    val repoMediator= MediatorLiveData<List<UserRepo>>()
 
+    val userInput =ObservableField<String>("")
 
+    val userRepoRepository =UserRepoRepository(repoRoomDatabase.userRepoDao())
 
     fun getUserRepos(user:String) {
         compositeDisposable.add(
             userRepository.getUserRepos(user).subscribe ({
                 val repoList =ArrayList<Repo>()
                 it.forEach {
+                    insertdata(it)
                     repoList.add(
                         Repo(
                             url =it.url.orEmpty(),
@@ -51,6 +64,40 @@ class SearchReposViewModel(
     fun onSearchClick(){
 
         getUserRepos(userInput.get().orEmpty())
+
+    }
+    
+    fun getDataRepoFromMediator(){
+        userRepoRepository.allRepo?.let {
+            repoMediator.addSource(it,{
+                //datos de la base de datos.
+             it.forEach{
+                 println("MediatorLiveData ${it.name}")
+             }
+            })
+        }
+    }
+
+
+    fun insertdata(githubReponse: GithubReponse)=viewModelScope.launch {
+        // cuando muchos viewmodel interactuen (GlobalScope)
+        userRepoRepository.insert(
+            UserRepo(
+                description = githubReponse.description.orEmpty(),
+                name = githubReponse.name.orEmpty(),
+                fullName = githubReponse.full_name.orEmpty(),
+                url = githubReponse.url.orEmpty()
+            )
+        )
+
+        /*
+        withContext(Dispatchers.Main){
+            activity,fragment, ui en general
+        }
+
+
+         */
+
 
     }
 
