dd403d41d27246d2c8e48a6f9116a10071a14e71
mobilestudio00 <jogarcia2411@gmal.com>
2020-02-01 13:46:28 -0600

    se agrego la base de datos

--- app/src/main/java/com/example/cleanmvvm/presentation/search/viewmodel/SearchReposViewModelFactory.kt
+++ app/src/main/java/com/example/cleanmvvm/presentation/search/viewmodel/SearchReposViewModelFactory.kt
@@ -2,14 +2,16 @@ package com.example.cleanmvvm.presentation.search.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.cleanmvvm.data.local.RepoRoomDatabase
 import com.example.cleanmvvm.domain.usecases.GitHubUserUseCase
 
 class SearchReposViewModelFactory(
-    val userRepository: GitHubUserUseCase
+    val userRepository: GitHubUserUseCase,
+    private val repoRoomDatabase: RepoRoomDatabase
     ):ViewModelProvider.NewInstanceFactory(){
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
 
-        return SearchReposViewModel(userRepository) as T
+        return SearchReposViewModel(userRepository,repoRoomDatabase) as T
     }
 }
